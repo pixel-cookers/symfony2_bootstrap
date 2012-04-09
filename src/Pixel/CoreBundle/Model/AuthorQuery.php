31ff3f442dcf3818f553a27e20ef011c1b6eb66e
Colin <bellino.colin@gmail.com>
2012-04-09 15:00:41 +0200

    Added some models (Book & Author) for later

---
+++ src/Pixel/CoreBundle/Model/AuthorQuery.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Pixel\CoreBundle\Model;
+
+use Pixel\CoreBundle\Model\om\BaseAuthorQuery;
+
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'author' table.
+ *
+ * 
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package    propel.generator.src.Pixel.CoreBundle.Model
+ */
+class AuthorQuery extends BaseAuthorQuery {
+
+} // AuthorQuery
