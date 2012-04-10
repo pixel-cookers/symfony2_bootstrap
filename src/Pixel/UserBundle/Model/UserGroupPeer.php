4f2788badef70b0ac097b7d13aa0597ec3fca72d
Colin <bellino.colin@gmail.com>
2012-04-10 18:52:22 +0200

    Added UserBundle models & schema

---
+++ src/Pixel/UserBundle/Model/UserGroupPeer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Pixel\UserBundle\Model;
+
+use Pixel\UserBundle\Model\om\BaseUserGroupPeer;
+
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'fos_user_group' table.
+ *
+ * 
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package    propel.generator.src.Pixel.UserBundle.Model
+ */
+class UserGroupPeer extends BaseUserGroupPeer {
+
+} // UserGroupPeer
---
+++ src/Pixel/UserBundle/Model/UserQuery.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Pixel\UserBundle\Model;
+
+use Pixel\UserBundle\Model\om\BaseUserQuery;
+
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'fos_user' table.
+ *
+ * 
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package    propel.generator.src.Pixel.UserBundle.Model
+ */
+class UserQuery extends BaseUserQuery {
+
+} // UserQuery
