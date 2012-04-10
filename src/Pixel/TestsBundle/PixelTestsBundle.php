f98579082057173d030d7a6d59191b0206860498
Colin <bellino.colin@gmail.com>
2012-04-10 21:11:23 +0200

    Adding the field LikeWaffles to the profile form

---
+++ src/Pixel/TestsBundle/PixelTestsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Pixel\TestsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PixelTestsBundle extends Bundle
+{
+}
---
+++ src/Pixel/UserBundle/Form/Type/ProfileFormType.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the FOSUserBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Pixel\UserBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+
+use FOS\UserBundle\Form\Type\ProfileFormType as BaseType;
+
+class ProfileFormType extends BaseType
+{
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+    }
+
+    public function getName()
+    {
+        return 'pixel_user_profile';
+    }
+
+    /**
+     * Builds the embedded form representing the user.
+     *
+     * @param FormBuilder $builder
+     * @param array       $options
+     */
+    protected function buildUserForm(FormBuilder $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('email', 'email')
+            ->add('like_waffles')
+        ;
+    }
+}
