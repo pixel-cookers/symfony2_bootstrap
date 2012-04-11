65c0cbf274ff8e763a8a8f66e68f50c12f0a1139
Colin <bellino.colin@gmail.com>
2012-04-11 14:58:06 +0200

    Updated Schema.xml and models files for add a many-to-many relation: user_book (which books user likes)

---
+++ src/Pixel/BootstrapBundle/Model/UserBook.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Pixel\BootstrapBundle\Model;
+
+use Pixel\BootstrapBundle\Model\om\BaseUserBook;
+
+
+/**
+ * Skeleton subclass for representing a row from the 'user_book' table.
+ *
+ * 
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package    propel.generator.src.Pixel.BootstrapBundle.Model
+ */
+class UserBook extends BaseUserBook {
+
+} // UserBook
---
+++ src/Pixel/BootstrapBundle/Model/UserBookQuery.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Pixel\BootstrapBundle\Model;
+
+use Pixel\BootstrapBundle\Model\om\BaseUserBookQuery;
+
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'user_book' table.
+ *
+ * 
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package    propel.generator.src.Pixel.BootstrapBundle.Model
+ */
+class UserBookQuery extends BaseUserBookQuery {
+
+} // UserBookQuery
